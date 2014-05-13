bcefa9aa4a6401ae8b5363fb1df73c9cb04d2d4a
melpp <sozerturk@gmail.com>
2014-05-12 21:48:58 -0500

    plot4

--- plot4.R
+++ plot4.R
@@ -13,7 +13,7 @@ datetime<-paste(plotData$Date,plotData$Time)
 datetime<-as.POSIXct(strptime(datetime, "%Y-%m-%d %H:%M:%S"))
 plotData<-cbind(plotData, datetime)
 
-plot4data<- subset(plotData,Global_active_power<0.5 )
+
 ##Create and save plots
 png(file = "plot4.png", height = 480, width = 480)
 par(mfrow=c(2,2), cex=0.5)
@@ -30,7 +30,7 @@ plot(plotData$datetime, plotData$Sub_metering_1, col="black",
      ylab= "Energy sub metering")
 lines(x=plotData$datetime, y=plotData$Sub_metering_2, col="red")
 lines(x=plotData$datetime, y=plotData$Sub_metering_3, col="blue")
-legend("topright", cex=0.2, lty=1, col=c("black","red", "blue"),
+legend("topright", cex=0.3, col=c("black","red", "blue"),
        legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
 
 #fourth plot
